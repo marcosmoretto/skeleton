c99481772ff0b75bbd67db081e219a0f51e63f2d
marcos <marcos@localhost.localdomain>
2020-03-24 19:38:46 -0300

    Sem cache

--- data/cache/module-classmap-cache.application.module.cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array (
-  'User\\Module' => '/home/unochapeco/Documentos/skeleton/module/User/Module.php',
-  'Core\\Module' => '/home/unochapeco/Documentos/skeleton/module/Core/Module.php',
-  'Sistema\\Module' => '/home/unochapeco/Documentos/skeleton/module/Sistema/Module.php',
-  'Usuarios\\Module' => '/home/unochapeco/Documentos/skeleton/module/Usuarios/Module.php',
-);
